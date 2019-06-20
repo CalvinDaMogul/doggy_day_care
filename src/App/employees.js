4c884aa1d2e5b105d5ef4960517aefce7337db34
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-20 07:18:48 -0500

    added employees

---
+++ src/App/employees.js
@@ -0,0 +1,31 @@
+const employees = [
+  {
+    id: 'staff1',
+    name: 'Joey',
+    imageUrl: 'https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492458862/articles/2013/11/13/netflix-and-marvel-s-luke-cage-and-a-brief-history-of-black-superheroes/131112-agard-cage-tease_jcirts',
+    position: 'Manager',
+  },
+
+  {
+    id: 'staff2',
+    name: 'Joey',
+    imageUrl: 'https://i.ytimg.com/vi/Wr5jeYwE4u4/hqdefault.jpg',
+    position: 'team member',
+  },
+
+  {
+    id: 'staff3',
+    name: 'Joey',
+    imageUrl: 'https://www.dailydot.com/wp-content/uploads/2018/06/black_marvel_superheroes_blade-800x377.png',
+    position: 'team member',
+  },
+
+  {
+    id: 'staff4',
+    name: 'Joey',
+    imageUrl: 'http://www.unleashthefanboy.com/wp-content/uploads/2014/08/miles-morales-630x400.jpg',
+    position: 'team member',
+  },
+];
+
+export default employees;
