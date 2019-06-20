961103d0c9056dc07a97c3c3f6039f1d612cb9be
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-19 20:23:42 -0500

    finished component

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import myDogs from './dogs';
+
+import DogComponent from '../components/DogComponent/DogComponent';
+
 
 class App extends React.Component {
+  state = {
+    dogs: [],
+  };
 
-};
+  componentDidMount() {
+    this.setState({ dogs: myDogs });
+  }
 
-componentDidMount() {
-
-}
-
-render() {
-  
+  render() {
+    const { dogs } = this.state;
+    return (
+    <div className="App">
+    <DogComponent dogs={dogs} />
+    </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,21 +1,29 @@
-import './goat.scss',
+import React from 'react';
+
+import dogShape from '../../helpers/propz/dogShape';
+
+import './Dog.scss';
+
 
 class Dog extends React.Component {
     static propTypes = {
-        dog: dogShape.dogShape,
+      dog: dogShape.dogShape,
     }
 
     render() {
-        const { dog } = this.props;
-        return (
-            <div className="Dog col-3">
-                <div className="card">
-                <img src={dog.imageUrl} className="card-img-top" alt=""/>
-                    <h5 className="card-title">{dog.name}</h5>
-                    <p className="card-text">{dog.color}</p>
-                    </div>
-                    </div>
-        )
+      const { dog } = this.props;
+      return (
+          <div className="dog col-3">
+              <div className="card">
+              <img src={dog.imageUrl} className="card-img-top" alt=""/>
+              <div className="card-body">
+                  <h5 className="card-title">{dog.name}</h5>
+                  <p className="card-text">{dog.gender}</p>
+              </div>
+              </div>
+          </div>
+
+      );
     }
 }
 
